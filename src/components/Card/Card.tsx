99ef7df21473bc07e5b529f5194664d435fb9601
Rabie Menad <rabie.menad07@outlook.com>
2022-07-26 16:50:21 +0200

    Improve the EnhancementAmelioration of the design

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,30 @@
 import { DataType } from "../../types/data";
 import styles from "./Card.module.scss";
+import {useTrail, animated } from "react-spring"
 
 
 export default function Card({ type, value }: DataType) {
 
-  return (
-   <>
-     <div className={styles.wrapper}>
-      <div className={styles.point_left}></div>
-      <div className={styles.point_right}></div>
-       <p className={styles.value}>{value}</p>
-     </div>
+   const trail = useTrail(1, {
+      from: { opacity: 0, x: 20},
+      to: { opacity: 1, x: 0}
+   })
 
-     <p className={styles.type}>{type}</p>
-   </>
+  return (
+     <>
+      {trail.map((cardStyle, index) => {
+
+         return <animated.div style={cardStyle} key={index}>
+
+            <div className={`${styles.wrapper} ${type}`}>
+               <div className={styles.point_left}></div>
+               <div className={styles.point_right}></div>
+               <p className={styles.value}>{value}</p>
+            </div>
+            
+            <p className={styles.type}>{type}</p>
+         </animated.div>
+      })}
+     </>
   )
 }
